c35c657a4b3746cab5dee4603faabbb41a345c3f
adriavieira314 <adriavieira731@gmail.com>
2022-06-22 22:57:31 -0400

    serviço de autenticacao

--- src/app/autenticacao/autenticacao.service.ts
+++ src/app/autenticacao/autenticacao.service.ts
@@ -1,20 +1,31 @@
+import { UsuarioService } from './usuario/usuario.service';
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { HttpClient, HttpResponse } from '@angular/common/http';
+import { Observable, tap } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AutenticacaoService {
-
   constructor(
-    private http: HttpClient
-  ) { }
+    private http: HttpClient,
+    private usuarioService: UsuarioService
+  ) {}
 
-  autenticar(usuario: string, senha: string): Observable<any> {
-    return this.http.post('http://localhost:3000/user/login', {
-      userName: usuario,
-      password: senha
-    });
+  // HttpResponse<any> = pega o todo, body, headers
+  autenticar(usuario: string, senha: string): Observable<HttpResponse<any>> {
+    return this.http.post(
+      'http://localhost:3000/user/login',
+      {
+        userName: usuario,
+        password: senha,
+      },
+      { observe: 'response' } // nao quero somente o body, quero o header da requisicao
+    ).pipe(
+      tap((response) => {
+        const authToken = response.headers.get('x-access-token') ?? ''; //recebendo o token
+        this.usuarioService.salvaToken(authToken); //salvando
+      })
+    );
   }
 }
