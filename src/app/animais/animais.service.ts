ea1837c019eddc6cbcc173071299c63506d07957
adriavieira314 <adriavieira731@gmail.com>
2022-06-26 20:37:20 -0400

    lista de animais por usuario/passando token

---
+++ src/app/animais/animais.service.ts
@@ -0,0 +1,20 @@
+import { TokenService } from './../autenticacao/token.service';
+import { Observable } from 'rxjs';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Animais } from './animal';
+
+const API = 'http://localhost:3000';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AnimaisService {
+  constructor(private http: HttpClient, private tokenService: TokenService) {}
+
+  listaDoUsuario(nomeUsuario: string): Observable<Animais> {
+    const token = this.tokenService.retornaToken();
+    const headers = new HttpHeaders().append('x-access-token', token);
+    return this.http.get<Animais>(`${API}/${nomeUsuario}/photos`, { headers }); //headers: headers
+  }
+}
