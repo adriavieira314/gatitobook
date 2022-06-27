ea814b649afca14760b8056f7713b229c8026ffd
adriavieira314 <adriavieira731@gmail.com>
2022-06-26 23:58:29 -0400

    auth guard - para quando usuario logado

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AuthGuard } from './autenticacao/auth/auth-guard.guard';
+import { LoginGuard } from './autenticacao/login-guard/login.guard';
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'home' },
   {
     path: 'home',
     loadChildren: () => import('./home/home.module').then((m) => m.HomeModule),
+    canLoad: [LoginGuard]
   },
   {
     path: 'animais',
