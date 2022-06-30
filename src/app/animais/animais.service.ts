742bed5c83b4082221ca7530853835182b30763a
adriavieira314 <adriavieira731@gmail.com>
2022-06-29 22:44:10 -0400

    servicos para excluir e curtir post

--- src/app/animais/animais.service.ts
+++ src/app/animais/animais.service.ts
@@ -1,11 +1,12 @@
 import { TokenService } from './../autenticacao/token.service';
-import { Observable } from 'rxjs';
+import { catchError, mapTo, Observable, of, throwError } from 'rxjs';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Animais, Animal } from './animal';
 import { environment } from 'src/environments/environment';
 
 const API = environment.apiUrl;
+const NOT_MODIFIED = '304'; // resposta para quando o usuario ja deu like naquela post
 
 @Injectable({
   providedIn: 'root',
@@ -19,5 +20,22 @@ export class AnimaisService {
 
   buscaPorId(id: number): Observable<Animal> {
     return this.http.get<Animal>(`${API}/photos/${id}`);
+  }
+
+  excluirPost(id: number): Observable<Animal> {
+    return this.http.delete<Animal>(`${API}/photos/${id}`);
+  }
+
+  curtirPost(id: number): Observable<boolean> {
+    return this.http.post(
+      `${API}/photos/${id}/likes`,
+      {},
+      { observe: 'response' } //vai passar a response inteira
+    ).pipe( // manipula o fluxo da requisicao
+      mapTo(true), // se a requisicao teve sucesso, retorna true
+      catchError((error) => {
+        return error.status === NOT_MODIFIED ? of(false) : throwError(error); // retorna falso se ja deu like ou a msg de error se deu erro
+      })
+    );
   }
 }
