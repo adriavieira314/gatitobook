b255b37c672480183c7d70f90d3a881ef4b7affe
adriavieira314 <adriavieira731@gmail.com>
2022-06-29 22:19:55 -0400

    registrando o interceptor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AutenticacaoModule } from './autenticacao/autenticacao.module';
 import { CabecalhoModule } from './components/cabecalho/cabecalho.module';
 import { RodapeModule } from './components/rodape/rodape.module';
 
@@ -16,7 +17,8 @@ import { RodapeModule } from './components/rodape/rodape.module';
     AppRoutingModule,
     HttpClientModule,
     CabecalhoModule,
-    RodapeModule
+    RodapeModule,
+    AutenticacaoModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/autenticacao/autenticacao.module.ts
+++ src/app/autenticacao/autenticacao.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { HeadersInterceptor } from './headers.interceptor';
 
 
 
@@ -7,6 +9,13 @@ import { CommonModule } from '@angular/common';
   declarations: [],
   imports: [
     CommonModule
+  ],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: HeadersInterceptor,
+      multi: true // multiplos interceptors
+    }
   ]
 })
 export class AutenticacaoModule { }
